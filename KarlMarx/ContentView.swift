c811a6d36595162cda457f58deec3df2a4c3b9e3
smatthewenglish <s.matthew.english@gmail.com>
2020-11-10 16:44:29 -0500

    Initial Commit

---
+++ KarlMarx/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KarlMarx
+//
+//  Created by S. Matthew English on 11/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KarlMarx/KarlMarxApp.swift
@@ -0,0 +1,17 @@
+//
+//  KarlMarxApp.swift
+//  KarlMarx
+//
+//  Created by S. Matthew English on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct KarlMarxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
